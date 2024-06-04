05465e3a01cb9175678f56562c5accce67747f9e
tcbear <84048696+tcbear@users.noreply.github.com>
2024-06-04 11:10:42 +0800

    Update testjavasourcecode20240528.java

--- testjavasourcecode20240528.java
+++ testjavasourcecode20240528.java
@@ -36,6 +36,44 @@ else:
    print("The factorial of",num,"is",factorial)
 # Python program to find the factorial of a number provided by the user
 # using recursion
+﻿# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+import java.io.*;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+# Python program to find the factorial of a number provided by the user.
+
+# change the value for a different result
+num = 7
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+# Python program to find the factorial of a number provided by the user
+# using recursion
 
 def factorial(x):
     """This is a recursive function
